0bfc64a456552be9edc7971507d12791916f281b
Darshni-05 <85580119+Darshni-05@users.noreply.github.com>
2023-06-25 20:07:42 +0530

    Time: 30 ms (32.75%), Space: 44.9 MB (52.03%) - LeetHub

--- 1031-add-to-array-form-of-integer/1031-add-to-array-form-of-integer.java
+++ 1031-add-to-array-form-of-integer/1031-add-to-array-form-of-integer.java
@@ -2,33 +2,53 @@ class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
         List<Integer> arr = new ArrayList<>();
 
-        for(int i=num.length-1;i>=0;i--){
-           // System.out.println(num[i]+"  "+k%10);
-            arr.add(0,num[i]+(k%10));
-            k=k/10;
-        }
-        if(k>0){
-            while (k!=0){
-                arr.add(0,k%10);
-                k/=10;
-            }
+        for(int i =num.length-1;i>=0;i--){
+            arr.add(0, (num[i]+k)%10);
+            k=(num[i]+k)/10;
         }
 
-        for(int i = arr.size()-1;i>=0;i--){
-            if(arr.get(i)>9&&i!=0)
-            {
-                int m = arr.get(i);
-                arr.set(i, m%10);
-                arr.set(i-1, arr.get(i-1)+m/10);
-            }
-            else if(arr.get(i)>9&&i==0)
-            {
-                int m = arr.get(i);
-                arr.set(i, m%10);
-                arr.add(0, m/10);
-            }
+        while(k!=0){
+            arr.add(0,k%10);
+            k/=10;
         }
+
+
+
 return arr;
+
+
+
+
+
+
+
+        // for(int i=num.length-1;i>=0;i--){
+        //    // System.out.println(num[i]+"  "+k%10);
+        //     arr.add(0,num[i]+(k%10));
+        //     k=k/10;
+        // }
+        // if(k>0){
+        //     while (k!=0){
+        //         arr.add(0,k%10);
+        //         k/=10;
+        //     }
+        // }
+
+        // for(int i = arr.size()-1;i>=0;i--){
+        //     if(arr.get(i)>9&&i!=0)
+        //     {
+        //         int m = arr.get(i);
+        //         arr.set(i, m%10);
+        //         arr.set(i-1, arr.get(i-1)+m/10);
+        //     }
+        //     else if(arr.get(i)>9&&i==0)
+        //     {
+        //         int m = arr.get(i);
+        //         arr.set(i, m%10);
+        //         arr.add(0, m/10);
+        //     }
+        // }
+        //     return arr;
 
 
 
