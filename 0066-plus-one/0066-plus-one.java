d1fcd8d0ac7cae0e4885d617d733b8a76f03ac67
Darshni-05 <85580119+Darshni-05@users.noreply.github.com>
2023-06-30 19:08:26 +0530

    Time: 3 ms (7.39%), Space: 41 MB (77.08%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+    //    int n = digits.length-1;
+    //    if(n>0){
+    //        if(digits[n]==9){
+    //        digits[n]=0;
+    //        while(n>=0 && digits[n--]==9)
+    //        digits[n]=0;
+    //        digits[n]+=1;
+    //        }
+    //        digits[n]+=1;
+    //        return digits;
+    //    } 
+    //    else if(digits[n]!=9){
+    //        digits[n]+=1;
+    //        return digits;
+    //    }
+    //    else{
+    //        int[] num = new int[2];
+    //        num[0]=1;
+    //        num[1]=0;
+    //        System.out.println(num);
+    //        return num;
+    //    }
+    // int res=0;
+    // for(int i=0;i<digits.length;i++) {
+    //      res=res*10+digits[i];
+    // }
+    // res=res+1;
+    // // int arr = new int
+    // // while(res!=0){
+
+    // // }
+    // int[] ans = Integer.toString(res).chars().map(c -> c-'0').toArray();
+    // return ans
+    
+    
+      List<Integer> arr = new ArrayList<>();
+        int k = 1;
+        for(int i =digits.length-1;i>=0;i--){
+            arr.add(0, (digits[i]+k)%10);
+            k=(digits[i]+k)/10;
+        }
+
+        while(k!=0){
+            arr.add(0,k%10);
+            k/=10;
+        }
+
+
+return  arr.stream().mapToInt(Integer::intValue).toArray();
+
+    }
+}
